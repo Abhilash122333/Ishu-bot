173d50e1edd2e794e5d3f27fa8554f55afeadd0d
Abhilash <93854823+Abhilash122333@users.noreply.github.com>
2022-10-09 07:32:19 +0530

    Update xeon.js

--- xeon.js
+++ xeon.js
@@ -78,7 +78,7 @@ async function startXeonBotInc() {
     const XeonBotInc = XeonBotIncConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Subscribe Xeon','Safari','1.0.0'],
+        browser: ['Subscribe Abhilash','chrome','1.0.0'],
         auth: state
     })
 
